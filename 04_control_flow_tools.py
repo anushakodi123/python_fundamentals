568e03649b09688676858eb4bb3aa85caa5ed192
anushakodi01234 <anushakodi01234@gmail.com>
2024-04-03 23:39:48 +0530

    Practising git fundamentals

---
+++ 04_control_flow_tools.py
@@ -0,0 +1,103 @@
+# if control flow
+x = int(input("Please enter an integer: "))
+
+if x < 0:
+    x = 0
+    print("Negative changed to zero")
+elif x == 0:
+    print("Zero")
+elif x == 1:
+    print("Single")
+else:
+    print("More")
+
+# for control flow
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
+
+#collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+    
+
+#range  
+for i in range(5):
+    print(i)
+
+print(list(range(-10, -100, -10)))
+
+print(range(10))
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found an odd number", num)
+
+# while True:
+#     pass
+
+# def http_error(status):
+#     match status:
+#         case 400:
+#             return "Bad request"
+#         case 404:
+#             return "Not found"
+#         case 418:
+#             return "I'm a teapot"
+#         case _:
+#             return "Something's wrong with the internet"
+
+# http_error(418)
+
+
+#functions
+
+def fib(n):
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+fib(2000)
+
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        reply = input(prompt)
+        if reply in {'y', 'ye', 'yes'}:
+            return True
+        if reply in {'n', 'no', 'nop', 'nope'}:
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
